985f7f86c0b0dc93f7cb996827f88b873b913049
946942303@qq.com <946942303@qq.com>
2019-07-15 21:38:40 +0800

    增加了：System.out.println("lalalal");

---
+++ PingShiTest/src/GitTest/testgit.java
@@ -0,0 +1,11 @@
+package GitTest;
+
+public class testgit {
+              public static void main(String[] args) {
+				   System.out.println("hello git project");
+				   System.out.println("hello git project");
+				   System.out.println("hello git project");
+				   System.out.println("hello git project");
+				   
+			}
+}
